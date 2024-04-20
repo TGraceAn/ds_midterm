a03726374a94e85d8c1b6387ef0f732c81966ee9
TGraceAn <annt.bi12-008@st.usth.edu.vn>
2024-04-20 18:49:30 +0200

    fix server bug

--- src/load_data.sh
+++ src/load_data.sh
@@ -1,5 +1,5 @@
 redis-server --port 1111 &
-cat config | redis-server - --port 2222 &
-cat config | redis-server - --port 3333 &
+cat redis.config | redis-server - --port 2222 &
+cat redis.config | redis-server - --port 3333 &
 
 redis-cli -p 1111 < actors.redis
